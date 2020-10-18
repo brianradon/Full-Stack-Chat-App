d917e8f874b0b907c4599d35e789e5f3479e5962
aeoliun <aeoliun@gmail.com>
2020-10-18 13:58:10 -0400

    initial heroku commit

--- scripts/Content.jsx
+++ scripts/Content.jsx
@@ -2,18 +2,13 @@
 import * as React from 'react';
 
 
-import { Button } from './Button';
-import { Socket } from './Socket';
 import ChatHistory from './ChatHistory';
 import { MessageForm } from './MessageForm';
-import Message from './Message';
 import "./App.css";
 import Navbar from './Navbar';
 import Sidebar from './Sidebar';
 
 export function Content() {
-
-    const [chatHistory, setChatHistory] = React.useState([]);
     
     return (
         <div>
